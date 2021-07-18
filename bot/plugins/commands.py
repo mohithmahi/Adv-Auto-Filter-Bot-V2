84c484a1168d15582aa89f03625285c31c7ef46f
mohithmahi <86865056+mohithmahi@users.noreply.github.com>
2021-07-18 18:21:55 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @mohithmahesh
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
